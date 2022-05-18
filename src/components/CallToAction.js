4529c4d527d6c9bc6f53851a969821b106b186d0
Arun Pratap Singh <arunpratapsingh1996@gmail.com>
2022-05-18 08:33:46 +0530

    checking if git is connected

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -2,7 +2,11 @@ import React from "react";
 
 export default function CallToAction({ btnText, others }) {
   return (
-    <button {...others} className="d-inline-flex align-items-center btn btn-lg text-white mt-2 px-4 bg--primary">
+    <button
+      {...others}
+      className="d-inline-flex align-items-center btn btn-lg text-white mt-2 px-4 bg--primary"
+      style={{ paddingTop: "0.65rem", paddingBottom: "0.65rem" }}
+    >
       {btnText}
       <i class="bi bi-arrow-right fs-4"></i>
     </button>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ import Seo from "../components/Seo";
 import logoPrimary from "../images/logo/logo-primary.png";
 import Fade from "react-reveal/Fade";
 import Zoom from "react-reveal/Zoom";
-import digital from "../svgs/undraw-digital-nomad.svg";
-import ContactForm from "../components/ContactForm";
+import digitalNomad from "../svgs/undraw-digital-nomad.svg";
+import results from "../svgs/undraw-results.svg";
 import CallToAction from "../components/CallToAction";
 
 const IndexPage = () => {
@@ -56,7 +56,7 @@ const IndexPage = () => {
               </div>
               <div className="col-12 col-md-7 col-lg-4 col-xl-5 text-white">
                 <Zoom>
-                  <img src={digital} className="img-fluid" />
+                  <img src={digitalNomad} className="img-fluid" />
                 </Zoom>
               </div>
             </div>
@@ -65,25 +65,27 @@ const IndexPage = () => {
 
         <div className="container-fluid py-5">
           <div className="container py-5">
-            <div className="row g-3 justify-content-center align-items-center">
-              <div className="col-12">
-                <h2 className="display-4 fw-bold text-start text-capitalize ">
+            <div className="row g-5 justify-content-between align-items-start">
+              <div className="col-12 col-md-7 col-lg-6 col-xl-7">
+                <h2 className="display-4 mb-4 fw-bold text-start ">
                   Our tech-driven services help your business build trust,
                   credibility and engage visitors.
                 </h2>
-              </div>
-              <div className="col-12">
                 <p className="fs-4 mb-4 text-secondary">
                   Combining our team and modern cutting-edge teachnology keeps
                   you one step ahead from your competition. Our web development
                   services help you create impact that drive success. We deliver
                   measurable and effective results for your business.
                 </p>
-                <p className="fs-4 mb-4 text-secondary">
-                  We are not just talking. We have the results to 
-                  <a href="#"> show</a>. Get in touch with us today and learn what makes
-                  appQue different from others web development agency.
+                <p className="fs-4 text-secondary">
+                  We are not just talking. We have the results to&nbsp;
+                  <a href="#">show</a>. Get in touch with us today and learn
+                  what makes appQue different from others web development
+                  agency.
                 </p>
+              </div>
+              <div className="col-12 col-md-5 col-lg-5 d-none d-md-block text-center">
+                <img src={results} alt="..." className="img-fluid" />
               </div>
             </div>
           </div>
@@ -94,20 +96,57 @@ const IndexPage = () => {
           style={{ backgroundColor: "rgb(235, 241, 255)" }}
         >
           <div className="container py-5">
-            <div className="row g-3 justify-content-center">
-              <div className="col-12 col-lg-8">
-                <h2 className="display-4 fw-bold text-capitalize text-sm-center">
+            <div className="row g-5 justify-content-between align-items-center">
+              <div className="col-12 col-md-7 col-lg-6">
+                <h2 className="display-4 mb-4 fw-bold text-start">
                   Ready to grow with us? Get your free proposal today
                 </h2>
-              </div>
-              <div className="col-12 col-lg-8 text-sm-center">
-                <p className="fs-4 mb-4 text-sm-center text-secondary">
-                  Our digital growth team have put otgether thousands of
-                  seccessfuldigital marketing campaigns for businesses looking
-                  to increase leads, phone calls, transactions, and qualified
-                  website traffic.
+                <p className="fs-4 mb-4 text-secondary">
+                  Our team closely analyse the successful competitors of your
+                  business. We put together all of that and create a success
+                  plan for your business. It unlocks more possibilites and
+                  engagements.
                 </p>
                 <CallToAction btnText="Request a proposal&nbsp;&nbsp;" />
+              </div>
+
+              <div className="col-12 col-md-5 col-lg-5 d-none d-md-block text-center">
+                <img src={results} alt="..." className="img-fluid" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* 
+        - Satisfied Customers [ 2 to 3 ] (optional)
+        - 4 to 6 Features with icon, heading and paragraph
+        - Services Listed heading, para and link to individual service page
+        - Talk in Numbers what we can help to establish (optional)
+        - large highlighted contact form with two column => 1:heading, paragraph,cta 2:image
+
+        */}
+        <div className="container-fluid py-5">
+          <div className="container py-5">
+            <div className="row g-5">
+              <div className="col-12 col-md-6">
+                <h2 className="display-4 mb-4 fw-bold text-start">
+                  What we can do for you?
+                </h2>
+                <p className="fs-4 mb-4 text-secondary">
+                  Our strength lies in the comprehensive suite of services we
+                  offer to our clients. From full-stack development to just
+                  consultancy- you demand, we deliver.
+                </p>
+              </div>
+              <div classname="col-12 col-md-6">
+                <h2 className="display-4 mb-4 fw-bold text-start">
+                  What we can do for you?
+                </h2>
+                <p className="fs-4 mb-4 text-secondary">
+                  Our strength lies in the comprehensive suite of services we
+                  offer to our clients. From full-stack development to just
+                  consultancy- you demand, we deliver.
+                </p>
               </div>
             </div>
           </div>
