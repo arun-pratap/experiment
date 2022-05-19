849b6b51000546e4f2d7f6a95df0068e47496064
Arun Pratap Singh <arunpratapsingh1996@gmail.com>
2022-05-19 14:30:00 +0530

    checking if git is connected

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import Layout from "./../components/Layout";
 import Seo from "../components/Seo";
+import CallToAction from "../components/CallToAction";
 import logoPrimary from "../images/logo/logo-primary.png";
 import Fade from "react-reveal/Fade";
 import Zoom from "react-reveal/Zoom";
 import digitalNomad from "../svgs/undraw-digital-nomad.svg";
 import results from "../svgs/undraw-results.svg";
-import CallToAction from "../components/CallToAction";
+import pexelsGrowth from "../images/pexelsShape.jpg";
+import pexelsWomen from "../images/pexelsWomen.jpg";
+
+// bg #101010
+// color #b3b3b3 !important
 
 const IndexPage = () => {
   const data = useStaticQuery(
@@ -37,10 +42,10 @@ const IndexPage = () => {
       />
       <Layout>
         {/* Cover Page */}
-        <div className="container-fluid py-5 bg--primary softbg--gradient-dark">
+        <div className="container-fluid py-5 bg--primary softbg--gradient-light">
           <div className="container py-4">
             <div className="row justify-content-center align-items-center g-5">
-              <div className="col-12 col-md-10 col-lg-8 col-xl-7 text-white">
+              <div className="col-12 col-md-10 col-lg-8 col-xl-7">
                 <Fade bottom>
                   <h1 className="display-2 mt-3 fw-bold">
                     Delivering Digital Experience
@@ -63,29 +68,33 @@ const IndexPage = () => {
           </div>
         </div>
 
-        <div className="container-fluid py-5">
+        <div className="container-fluid py-5" style={{backgroundColor:"#0f0f0f",color:"#b3b3b3"}}>
           <div className="container py-5">
-            <div className="row g-5 justify-content-between align-items-start">
+            <div className="row g-5 justify-content-between align-items-center">
               <div className="col-12 col-md-7 col-lg-6 col-xl-7">
-                <h2 className="display-4 mb-4 fw-bold text-start ">
+                <h2 className="display-4 mb-4 fw-bold text-start text-white">
                   Our tech-driven services help your business build trust,
                   credibility and engage visitors.
                 </h2>
-                <p className="fs-4 mb-4 text-secondary">
-                  Combining our team and modern cutting-edge teachnology keeps
-                  you one step ahead from your competition. Our web development
-                  services help you create impact that drive success. We deliver
-                  measurable and effective results for your business.
+                <p className="fs-4 mb-4">
+                  Combining our team's experience in web development services
+                  and modern cutting-edge technology keeps you one step ahead of
+                  your competition. Our web development services help you create
+                  an impact that drives success. We deliver measurable and
+                  effective results for your business.
                 </p>
-                <p className="fs-4 text-secondary">
-                  We are not just talking. We have the results to&nbsp;
-                  <a href="#">show</a>. Get in touch with us today and learn
-                  what makes appQue different from others web development
-                  agency.
+                <p className="fs-4">
+                  We have the experience and demonstrated&nbsp;
+                  <a href="#">results</a> to show you how we can help with your
+                  web development project. Contact us today to schedule a free
+                  consultation.
                 </p>
               </div>
               <div className="col-12 col-md-5 col-lg-5 d-none d-md-block text-center">
-                <img src={results} alt="..." className="img-fluid" />
+                <div className="tech-background-video"></div>
+                <div>
+                  <img src={pexelsGrowth} alt="..." className="img-fluid" />
+                </div>
               </div>
             </div>
           </div>
@@ -102,22 +111,27 @@ const IndexPage = () => {
                   Ready to grow with us? Get your free proposal today
                 </h2>
                 <p className="fs-4 mb-4 text-secondary">
-                  Our team closely analyse the successful competitors of your
-                  business. We put together all of that and create a success
-                  plan for your business. It unlocks more possibilites and
-                  engagements.
+                  Our team closely analyzes the successful strategies of your
+                  competitors. We combine this knowledge with our expertise and
+                  create a success plan for your business that unlocks more
+                  possibilities and engagements.
                 </p>
                 <CallToAction btnText="Request a proposal&nbsp;&nbsp;" />
               </div>
 
               <div className="col-12 col-md-5 col-lg-5 d-none d-md-block text-center">
-                <img src={results} alt="..." className="img-fluid" />
+                <div className="rtg">
+                  <img src={pexelsWomen} alt="..." className="img-fluid" />
+                </div>
               </div>
             </div>
           </div>
         </div>
 
-        {/* 
+        {/* @TODO
+        - Cover Page
+        - Hook headline
+        - Contact Form
         - Satisfied Customers [ 2 to 3 ] (optional)
         - 4 to 6 Features with icon, heading and paragraph
         - Services Listed heading, para and link to individual service page
@@ -143,9 +157,9 @@ const IndexPage = () => {
                   What we can do for you?
                 </h2>
                 <p className="fs-4 mb-4 text-secondary">
-                  Our strength lies in the comprehensive suite of services we
-                  offer to our clients. From full-stack development to just
-                  consultancy- you demand, we deliver.
+                  Our strength lies in the full range of services we offer to
+                  our clients. From full-stack development to just consultancy,
+                  we can handle all your needs.
                 </p>
               </div>
             </div>
