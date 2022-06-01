577e7456dbb4f871f1be9aebedbf0e7d78b2274c
Arun Pratap Singh <arunpratapsingh1996@gmail.com>
2022-06-02 00:50:40 +0530

    minor changes

---
+++ src/pages/pujadas.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+function Seo({ title, image, description, slug, type }) {
+  const baseURL ="https://www.appque.in";
+  const canonicalURL = `https://www.appque.in/${slug}`;
+  //  main url of a particular webpage
+  const URL = `${baseURL}/${slug}`;
+
+  return (
+    <Helmet>
+      {/* canonical url: anys related , original or old url */}
+      <link rel="canonical" href={canonicalURL} />
+
+      {/* <!-- Primary Meta Tags --> */}
+      <title>{title}</title>
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
+
+      {/* <!-- Open Graph / Facebook --> */}
+      {/* type can be "website", "blog" or "article" */}
+      <meta property="og:type" content={type} />
+      <meta property="og:url" content={URL} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+
+      {/* <!-- Twitter --> */}
+      {/* summary_large_image is must to preview the card with large image */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={URL} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={image} />
+    </Helmet>
+  );
+}
+
+export default function Pujadas() {
+  return (
+ <main>
+     <section>
+         </section>
+ </main>
+  )
+}
