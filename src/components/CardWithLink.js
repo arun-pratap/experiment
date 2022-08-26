226553df3643b72d0aeda6e0498fa227ff6deab7
Arun Pratap Singh <arunpratapsingh1996@gmail.com>
2022-08-26 20:18:04 +0530

    homepage done

--- src/components/CardWithLink.js
+++ src/components/CardWithLink.js
@@ -15,13 +15,13 @@ export default function Card({ title, para, img, imgAlt, link }) {
                 />
             </div>
             <div className="card-body">
-                <h5 className="fs-3 mt-1 mb-2 fw--700">
+                <h5 className="fs-2 mt-1 mb-2 fw--700">
                     {title}
                 </h5>
                 <p className="fs-5 text--dark">{para}</p>
                 <a
                     href={`/${link}`}
-                    className="d-inline-flex align-items-center btn btn-primary text-white mt-2 px-4 fs-6 fw-600 text-uppercase"
+                    className="d-inline-flex align-items-center btn btn-primary text-white mt-2 px-4 fs-6 fw--600 text-uppercase"
                     style={{ paddingTop: "0.60rem", paddingBottom: "0.75rem" }}
                 >
                     Learn More &nbsp;
